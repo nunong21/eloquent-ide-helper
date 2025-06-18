9adfdba5ecb53b5322fed9e6fc8c047038f3db2a
Nuno Almeida <nuno.almeida@visma.com>
2025-06-18 11:14:59 +0100

    Add config and view

--- src/Commands/GenerateDocsModelsCommand.php
+++ src/Commands/GenerateDocsModelsCommand.php
@@ -10,24 +10,25 @@ use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\ConsoleOutput;
 use Symfony\Component\Console\Output\OutputInterface;
 
+
 class GenerateDocsModelsCommand extends AbstractCommand {
-	public function __construct() {
-		parent::__construct('generate-docs-models');
-	}
+    public function __construct() {
+        parent::__construct('generate-docs-models');
+    }
 
-	public function configure() {
-		$this->setAliases(['models']);
-	}
+    public function configure() {
+        $this->setAliases(['models']);
+    }
 
-	protected function execute(InputInterface $input, OutputInterface $output) {
-		$this->style->title('Writing documentation to models');
+    protected function execute(InputInterface $input, OutputInterface $output): int {
+        $this->style->title('Writing documentation to models');
 
-		$fileSystem = new Filesystem();
-		$command = new ModelsCommand($fileSystem);
-		$command->setLaravel(Container::getInstance());
-		return $command->run(
-			new ArrayInput(['--write' => true, '--reset' => true]),
-			new ConsoleOutput()
-		);
-	}
+        $fileSystem = new Filesystem();
+        $command = new ModelsCommand($fileSystem);
+        $command->setLaravel(Container::getInstance());
+        return $command->run(
+            new ArrayInput(['--write' => true, '--reset' => true]),
+            new ConsoleOutput()
+        );
+    }
 }
